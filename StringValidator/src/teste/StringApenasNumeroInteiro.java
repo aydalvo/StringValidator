a4db3ac5877d1a4b0ae282750dbee4effed0f6a1
Aydalvo <aydalvo.nery@gmail.com>
2013-06-04 22:45:51 -0300

    Segundo commit
    Atualizacao

--- StringValidator/src/teste/StringApenasNumeroInteiro.java
+++ StringValidator/src/teste/StringApenasNumeroInteiro.java
@@ -7,76 +7,109 @@ import validator.Validacao;
 
 public class StringApenasNumeroInteiro extends StringValidator {
 	
-	Validacao validacao = new Validacao();
+	@Test
+	public void apenasLetras() {
+		this.setFrase(" é apenas letra? ");
+		this.setParametro("aaa([foo])aaa");
+		Assert.assertFalse(getValidacao().isStringSemLetras(this.getParametro(), PATTERN_APENAS_LETRAS, this.getFrase()));
+	}
+	
+	@Test
+	public void apenasLetras2() {
+		this.setFrase(" é apenas letra? ");
+		this.setParametro("aaa foo aaa");
+		Assert.assertTrue(getValidacao().isStringSemLetras(this.getParametro(), PATTERN_APENAS_LETRAS, this.getFrase()));
+	}
+	
+	@Test
+	public void bloqueiaEspacoNoFinal() {
+		this.setFrase(" é apenas letra? ");
+		this.setParametro("aaa fooaaa ");
+		Assert.assertFalse(getValidacao().isStringSemLetras(this.getParametro(), PATTERN_APENAS_LETRAS, this.getFrase()));
+	}
+	
+	@Test
+	public void bloqueiaEspacosDuplos() {
+		this.setFrase(" palavra sem espaços duplos? ");
+		this.setParametro("aaa  fooaaa");
+		Assert.assertFalse(getValidacao().isStringSemLetras(this.getParametro(), PATTERN_APENAS_LETRAS, this.getFrase()));
+	}
 	
 	@Test
 	public void apenasNumeroNatural() {
 		this.setFrase(" é apenas número? ");
 		this.setParametro("1234567890");
-		Assert.assertTrue(validacao.isStringSemLetras(this.getParametro(), PATTERN_APENAS_NUMEROS_INTEIROS, this.getFrase()));
+		Assert.assertTrue(getValidacao().isStringSemLetras(this.getParametro(), PATTERN_APENAS_NUMEROS_INTEIROS, this.getFrase()));
 	}
 	
 	@Test
 	public void bloqueiaNumeroComDigitos() {
 		this.setFrase(" é número com digito? ");
 		this.setParametro("123.45");
-		Assert.assertFalse(validacao.isStringSemLetras(this.getParametro(), PATTERN_APENAS_NUMEROS_INTEIROS, this.getFrase()));
+		Assert.assertFalse(getValidacao().isStringSemLetras(this.getParametro(), PATTERN_APENAS_NUMEROS_INTEIROS, this.getFrase()));
 	}
 	
 	@Test
 	public void bloqueiaStringEmpty() {
 		this.setFrase(" é número com digito? ");
 		this.setParametro("");
-		Assert.assertFalse(validacao.isStringSemLetras(this.getParametro(), PATTERN_APENAS_NUMEROS_INTEIROS, this.getFrase()));
+		Assert.assertFalse(getValidacao().isStringSemLetras(this.getParametro(), PATTERN_APENAS_NUMEROS_INTEIROS, this.getFrase()));
 	}
 	
 	@Test
 	public void bloqueiaStringNula() {
 		this.setFrase(" é número com digito? ");
 		this.setParametro(null);
-		Assert.assertFalse(validacao.isStringSemLetras(this.getParametro(), PATTERN_APENAS_NUMEROS_INTEIROS, this.getFrase()));
+		Assert.assertFalse(getValidacao().isStringSemLetras(this.getParametro(), PATTERN_APENAS_NUMEROS_INTEIROS, this.getFrase()));
 	}
 	
 	@Test
 	public void bloqueiaStringAlfaNumericaLower() {
 		this.setFrase(" é número com digito? ");
 		this.setParametro("123a");
-		Assert.assertFalse(validacao.isStringSemLetras(this.getParametro().toLowerCase(), PATTERN_APENAS_NUMEROS_INTEIROS, this.getFrase()));
+		Assert.assertFalse(getValidacao().isStringSemLetras(this.getParametro().toLowerCase(), PATTERN_APENAS_NUMEROS_INTEIROS, this.getFrase()));
 	}
 	
 	@Test
 	public void bloqueiaStringAlfaNumericaUpper() {
 		this.setFrase(" é número com digito? ");
 		this.setParametro("123a");
-		Assert.assertFalse(validacao.isStringSemLetras(this.getParametro().toUpperCase(), PATTERN_APENAS_NUMEROS_INTEIROS, this.getFrase()));
+		Assert.assertFalse(getValidacao().isStringSemLetras(this.getParametro().toUpperCase(), PATTERN_APENAS_NUMEROS_INTEIROS, this.getFrase()));
 	}
 	
 	@Test
 	public void bloqueiaLetra() {
 		this.setFrase(" é número com digito? ");
 		this.setParametro("John");
-		Assert.assertFalse(validacao.isStringSemLetras(this.getParametro().toUpperCase(), PATTERN_APENAS_NUMEROS_INTEIROS, this.getFrase()));
+		Assert.assertFalse(getValidacao().isStringSemLetras(this.getParametro().toUpperCase(), PATTERN_APENAS_NUMEROS_INTEIROS, this.getFrase()));
+	}
+	
+	@Test
+	public void aceitaNumeroInteiroComoDecimal() {
+		this.setFrase(" é número sem digito válido? ");
+		this.setParametro("12350");
+		Assert.assertTrue(getValidacao().isStringSemLetras(this.getParametro(), PATTERN_APENAS_NUMEROS_COM_DIGITOS, this.getFrase()));
 	}
 	
 	@Test
 	public void bloqueiaFormatacaoErrada() {
 		this.setFrase(" é número com digito válido? ");
 		this.setParametro("123..50");
-		Assert.assertFalse(validacao.isStringSemLetras(this.getParametro().toUpperCase(), PATTERN_APENAS_NUMEROS_COM_DIGITOS, this.getFrase()));
+		Assert.assertFalse(getValidacao().isStringSemLetras(this.getParametro().toUpperCase(), PATTERN_APENAS_NUMEROS_COM_DIGITOS, this.getFrase()));
 	}
 	
 	@Test
 	public void bloqueiaFormatacaoMilhar() {
 		this.setFrase(" é número com digito válido? ");
 		this.setParametro("1.234.50");
-		Assert.assertFalse(validacao.isStringSemLetras(this.getParametro().toUpperCase(), PATTERN_APENAS_NUMEROS_COM_DIGITOS, this.getFrase()));
+		Assert.assertFalse(getValidacao().isStringSemLetras(this.getParametro().toUpperCase(), PATTERN_APENAS_NUMEROS_COM_DIGITOS, this.getFrase()));
 	}
 	
 	@Test
 	public void bloqueiaComecarComPonto() {
 		this.setFrase(" é número com digito válido? ");
 		this.setParametro(".23450");
-		Assert.assertFalse(validacao.isStringSemLetras(this.getParametro().toUpperCase(), PATTERN_APENAS_NUMEROS_COM_DIGITOS, this.getFrase()));
+		Assert.assertFalse(getValidacao().isStringSemLetras(this.getParametro().toUpperCase(), PATTERN_APENAS_NUMEROS_COM_DIGITOS, this.getFrase()));
 	}
 	
 	
--- StringValidator/src/teste/StringValidator.java
+++ StringValidator/src/teste/StringValidator.java
@@ -2,17 +2,19 @@ package teste;
 
 public class StringValidator {
 
+	protected static String PATTERN_APENAS_LETRAS = "[a-zA-Z]+(\\p{Blank}{0,1}[a-zA-Z]+)*";
 	protected static String PATTERN_APENAS_NUMEROS_INTEIROS = "\\d*[^a-zA-Z]";
-	protected static String PATTERN_APENAS_NUMEROS_COM_DIGITOS = "\\d+[p{.}]\\d*[^a-zA-Z]";
+	protected static String PATTERN_APENAS_NUMEROS_COM_DIGITOS = "\\d+[p{.}]{0,1}\\d*[^a-zA-Z]";
 	
 	private String parametro;
 	private String frase;
 	
-	public static String getPATTERN_APENAS_NUMEROS_INTEIROS() {
-		return PATTERN_APENAS_NUMEROS_INTEIROS;
+	public Validacao getValidacao() {
+		return criaValidacao();
 	}
-	public static String getPATTERN_APENAS_NUMEROS_COM_DIGITOS() {
-		return PATTERN_APENAS_NUMEROS_COM_DIGITOS;
+	
+	public Validacao criaValidacao() {
+		return new Validacao();
 	}
 	
 	public String getParametro() {
